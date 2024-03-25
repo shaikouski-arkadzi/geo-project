c020b6d0098eb2c84e0e56f9e45e216ca585a66e
Arkadzi_Shaikouski <arkadiy92@gmail.com>
2024-03-25 14:31:03 +0300

    added leaflet library for viewing map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useCities } from '../state/Cities.context';
 import styles from './Map.module.css';
 
 function Map() {
   const navigate = useNavigate();
+
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const { cities } = useCities();
+
   const lat = searchParams.get('lat');
   const lng = searchParams.get('lng');
   return (
     <div className={styles.mapContainer}>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
+      <MapContainer center={mapPosition} zoom={6} scrollWheelZoom className={styles.map}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map(city => (
+          <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
